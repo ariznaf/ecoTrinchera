18cdcf75831c0932f90b0d867cb3f768aab433b6
ariznaf <fernando@arizna-alzu.org>
2026-02-15 00:22:44 +0100

    integración de gitcus

---
+++ js/giscus.js
@@ -0,0 +1,12 @@
+const giscus = document.createElement("script");
+giscus.src = "https://giscus.app/client.js";
+giscus.setAttribute("data-repo", "TU-USUARIO/giscus-comentarios");
+giscus.setAttribute("data-repo-id", "XXXXXXXX");
+giscus.setAttribute("data-category", "EcoTrinchera");
+giscus.setAttribute("data-category-id", "YYYYYYYY");
+giscus.setAttribute("data-mapping", "pathname");
+giscus.setAttribute("data-theme", "light");
+giscus.crossOrigin = "anonymous";
+giscus.async = true;
+
+document.getElementById("giscus").appendChild(giscus);
