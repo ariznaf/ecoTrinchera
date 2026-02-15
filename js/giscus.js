bc3e7c887ea56c19d535c78eb991d9ba97f569a3
ariznaf <fernando@arizna-alzu.org>
2026-02-15 01:21:53 +0100

    config giscus js

--- js/giscus.js
+++ js/giscus.js
@@ -1,12 +1,14 @@
 const giscus = document.createElement("script");
 giscus.src = "https://giscus.app/client.js";
+
 giscus.setAttribute("data-repo", "ariznaf/giscus-comentarios");
-giscus.setAttribute("data-repo-id", "XXXXXXXX");
 giscus.setAttribute("data-category", "EcoTrinchera");
-giscus.setAttribute("data-category-id", "YYYYYYYY");
+
 giscus.setAttribute("data-mapping", "pathname");
-giscus.setAttribute("data-theme", "light");
+giscus.setAttribute("data-reactions-enabled", "1");
+giscus.setAttribute("data-emit-metadata", "0");
+giscus.setAttribute("data-theme", "preferred_color_scheme");
 giscus.crossOrigin = "anonymous";
 giscus.async = true;
 
-document.getElementById("giscus").appendChild(giscus);
+document.getElementById("giscus")?.appendChild(giscus);
